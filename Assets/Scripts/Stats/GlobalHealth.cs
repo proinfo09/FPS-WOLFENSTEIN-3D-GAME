3cbdb5d9c3bb1db855854da35ae176ca3c062531
proinfo09 <63229875+proinfo09@users.noreply.github.com>
2022-04-27 16:18:49 +0700

    update ui

--- Assets/Scripts/Stats/GlobalHealth.cs
+++ Assets/Scripts/Stats/GlobalHealth.cs
@@ -9,6 +9,10 @@ public class GlobalHealth : MonoBehaviour
     public GameObject healthDisplay;
     public static int healthValue;
     public int interalHealth;
+    public GameObject hp100;
+    public GameObject hp75;
+    public GameObject hp50;
+    public GameObject hp25;
 
     private void Start()
     {
@@ -24,5 +28,33 @@ public class GlobalHealth : MonoBehaviour
         }
         interalHealth = healthValue;
         healthDisplay.GetComponent<Text>().text = "" + healthValue;
+        if(healthValue >= 75)
+        {
+            hp100.SetActive(true);
+            hp75.SetActive(false);
+            hp50.SetActive(false);
+            hp25.SetActive(false);
+        }
+        if (healthValue < 25)
+        {
+            hp100.SetActive(false);
+            hp75.SetActive(false);
+            hp50.SetActive(false);
+            hp25.SetActive(true);
+        }
+        if (healthValue >= 25 && healthValue < 50)
+        {
+            hp100.SetActive(false);
+            hp75.SetActive(false);
+            hp50.SetActive(true);
+            hp25.SetActive(false);
+        }
+        if(healthValue >= 50 && healthValue < 75)
+        {
+            hp100.SetActive(false);
+            hp75.SetActive(true);
+            hp50.SetActive(false);
+            hp25.SetActive(false);
+        }
     }
 }
