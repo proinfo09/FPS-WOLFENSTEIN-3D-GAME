ee215961aaf47c2fe1b8462097593ee164cde8f8
proinfo09 <63229875+proinfo09@users.noreply.github.com>
2022-04-27 15:30:52 +0700

    add ui fade in/fade out, image gun

--- Assets/Scripts/Enemies/EnemyDeath.cs
+++ Assets/Scripts/Enemies/EnemyDeath.cs
@@ -24,6 +24,7 @@ public class EnemyDeath : MonoBehaviour
             enemyAI.SetActive(false);
             theEnemy.GetComponent<LookPlayer>().enabled = false;
             this.GetComponent<BoxCollider>().isTrigger = true;
+            GlobalScore.scoreValue += 100;
         }
     }
 }
--- Assets/Scripts/Weapons/HandGunPickup.cs
+++ Assets/Scripts/Weapons/HandGunPickup.cs
@@ -9,6 +9,7 @@ public class HandGunPickup : MonoBehaviour
     public GameObject pickupHandgun;
     public AudioSource handgunPickupSound;
     public GameObject pickUpDisplay;
+    public GameObject pistolImage;
 
     void OnTriggerEnter(Collider other)
     {
@@ -19,5 +20,6 @@ public class HandGunPickup : MonoBehaviour
         pickUpDisplay.SetActive(false);
         pickUpDisplay.GetComponent<Text>().text = "HANDGUN";
         pickUpDisplay.SetActive(true);
+        pistolImage.SetActive(true);
     }
 }
